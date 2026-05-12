fd8bc12d8aa7f4a5d7b554d8ed0a5b2ebb7f56eb
riobijoy <75292942+riobijoy@users.noreply.github.com>
2026-05-12 19:10:25 +0530

    Update fetch_weather.py

--- fetch_weather.py
+++ fetch_weather.py
@@ -3,37 +3,51 @@ import json
 from datetime import datetime
 import os
 
-# FTP Credentials from GitHub Secrets
+# FTP Credentials
 HOST = os.getenv("FTP_HOST")
 USER = os.getenv("FTP_USER")
 PASSWORD = os.getenv("FTP_PASS")
 
-# FTP Connection
+# Connect FTP
 ftp = FTP(HOST)
 ftp.login(USER, PASSWORD)
 
-# Weather file name
-filename = "weather.txt"
+# Go to folder
+ftp.cwd("/NHPASSAM")
 
-# Download file
-with open(filename, "wb") as file:
-    ftp.retrbinary(f"RETR {filename}", file.write)
+# Get all files
+files = ftp.nlst()
+
+# Keep only CSV files
+csv_files = [f for f in files if f.endswith(".csv")]
+
+# Sort latest file
+csv_files.sort(reverse=True)
+
+# Latest file
+latest_file = csv_files[0]
+
+print("Latest File:", latest_file)
+
+# Download latest file
+with open(latest_file, "wb") as file:
+    ftp.retrbinary(f"RETR {latest_file}", file.write)
 
 ftp.quit()
 
-# Read weather data
-with open(filename, "r") as file:
+# Read CSV content
+with open(latest_file, "r") as file:
     raw_data = file.read()
 
 # Create JSON
 data = {
-    "station": "Assam Weather Station",
-    "weather_data": raw_data,
-    "updated_time": str(datetime.now())
+    "latest_file": latest_file,
+    "updated_time": str(datetime.now()),
+    "raw_weather_data": raw_data
 }
 
 # Save JSON
 with open("latest.json", "w") as json_file:
     json.dump(data, json_file, indent=4)
 
-print("Weather updated successfully")
+print("Weather data updated successfully")
